0069313842b333f981cdb7bdf52ec3a5e4f19913
Amin Pathan <aminpathan802@gmail.com>
2025-09-20 10:19:38 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,23 +12,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+uh9um*ets@$1bn^2n16hsw+%r3a0u08$t6!vlc+--u_k!@sj6'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -70,8 +66,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dhrms.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
@@ -81,8 +76,7 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,8 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -112,8 +105,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 
 STATIC_URL = '/static/'
 
@@ -123,8 +115,7 @@ STATICFILES_DIRS = [
 ]
 
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -132,10 +123,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 BASE_URL = "https://2cxm7jdq-8000.inc1.devtunnels.ms"
 
 
-# Google Maps API Key
+
 GOOGLE_MAPS_API_KEY = "AIzaSyCEhoQNJlqeXBcTyHPAEYIzOQgIvdvGN7M"
 
-ABDM_BASE_URL = "https://dev.abdm.gov.in/gateway/v0.5"   # Example (use sandbox endpoint)
+ABDM_BASE_URL = "https://dev.abdm.gov.in/gateway/v0.5"   
 ABDM_CLIENT_ID = "your-sandbox-client-id"
 ABDM_CLIENT_SECRET = "your-sandbox-client-secret"
 
